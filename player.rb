f1e1d9ffc3f569697183251d598bd180262e902f
Alexander Horvath <tantousha@me.com>
2018-09-10 18:14:02 -0700

    Added and tested player lives

--- player.rb
+++ player.rb
@@ -1,6 +1,18 @@
 class Player
-  def initilize(name)
+  attr_reader :name, :lives, :alive
+
+  def initialize name
     @name = name
     @lives = 3
+    @alive = true
 
   end
+
+  def decreaseLives num
+    @lives = @lives - num
+    if (@lives == 0)
+      @alive = false
+    end
+  end
+
+end
