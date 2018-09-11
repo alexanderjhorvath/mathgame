5281c684c4fc281a9cb79c9416678012f7ea7d73
Alexander Horvath <tantousha@me.com>
2018-09-10 18:41:03 -0700

    Created question creator and answer checker methods

--- main.rb
+++ main.rb
@@ -1,9 +1,10 @@
-require './game'
+# require './game'
 require './player'
 require './question'
 
 player1 = Player.new("Player 1")
 player2 = Player.new("Player 2")
+question = Question.new
 
 player1.decreaseLives 2
 puts player1.lives
@@ -11,3 +12,7 @@ puts player1.alive
 player1.decreaseLives 1
 puts player1.lives
 puts player1.alive
+
+puts question.question
+question.answerChecker 15
+puts question.result
